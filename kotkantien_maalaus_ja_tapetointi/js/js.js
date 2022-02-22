0fa5a3f542e9699a6fe764ff0302ba57c56d3e46
samieskonniemi <100156434+samieskonniemi@users.noreply.github.com>
2022-02-22 12:49:35 +0200

    Add files via upload

---
+++ kotkantien_maalaus_ja_tapetointi/js/js.js
@@ -0,0 +1,205 @@
+//Change wall colors start
+function changeWallColor(clicked_id) {
+    if (clicked_id == "colorFiesta1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#DD4132");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#DD4132");
+    }
+    else if (clicked_id == "colorJesterRed1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#9E1030");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#9E1030");
+    }
+    else if (clicked_id == "colorTurmenic1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#FE840E");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#FE840E");
+    }
+    else if (clicked_id == "colorLivingCoral1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#FF6F61");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#FF6F61");
+    }
+    else if (clicked_id == "colorPinkPeacock1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#C62168");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#C62168");
+    }
+    else if (clicked_id == "colorPepperStem1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#8D9440");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#8D9440");
+    }
+    else if (clicked_id == "colorAspenGold1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#FFD662");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#FFD662");
+    }
+    else if (clicked_id == "colorPrincessBlue1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#00539C");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#00539C");
+    }
+    else if (clicked_id == "colorToffee1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#755139");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#755139");
+    }
+    else if (clicked_id == "colorMangoMojito1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#D69C2F");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#D69C2F");
+    }
+    else if (clicked_id == "colorTerrariumMoss1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#616247");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#616247");
+    }
+    else if (clicked_id == "colorSweetLilac1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#E8B5CE");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#E8B5CE");
+    }
+    else if (clicked_id == "colorSoybean1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#D2C29D");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#D2C29D");
+    }
+    else if (clicked_id == "colorEclipse1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#343148");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#343148");
+    }
+    else if (clicked_id == "colorSweetCorn1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#F0EAD6");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#F0EAD6");
+    }
+    else if (clicked_id == "colorBrownGranite1") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#615550");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#615550");
+    }
+    else if (clicked_id == "colorFiesta2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#DD4132");
+        document.getElementById("_RightTop_").setAttribute("fill", "#DD4132");
+    }
+    else if (clicked_id == "colorJesterRed2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#9E1030");
+        document.getElementById("_RightTop_").setAttribute("fill", "#9E1030");
+    }
+    else if (clicked_id == "colorTurmenic2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#FE840E");
+        document.getElementById("_RightTop_").setAttribute("fill", "#FE840E");
+    }
+    else if (clicked_id == "colorLivingCoral2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#FF6F61");
+        document.getElementById("_RightTop_").setAttribute("fill", "#FF6F61");
+    }
+    else if (clicked_id == "colorPinkPeacock2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#C62168");
+        document.getElementById("_RightTop_").setAttribute("fill", "#C62168");
+    }
+    else if (clicked_id == "colorPepperStem2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#8D9440");
+        document.getElementById("_RightTop_").setAttribute("fill", "#8D9440");
+    }
+    else if (clicked_id == "colorAspenGold2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#FFD662");
+        document.getElementById("_RightTop_").setAttribute("fill", "#FFD662");
+    }
+    else if (clicked_id == "colorPrincessBlue2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#00539C");
+        document.getElementById("_RightTop_").setAttribute("fill", "#00539C");
+    }
+    else if (clicked_id == "colorToffee2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#755139");
+        document.getElementById("_RightTop_").setAttribute("fill", "#755139");
+    }
+    else if (clicked_id == "colorMangoMojito2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#D69C2F");
+        document.getElementById("_RightTop_").setAttribute("fill", "#D69C2F");
+    }
+    else if (clicked_id == "colorTerrariumMoss2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#616247");
+        document.getElementById("_RightTop_").setAttribute("fill", "#616247");
+    }
+    else if (clicked_id == "colorSweetLilac2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#E8B5CE");
+        document.getElementById("_RightTop_").setAttribute("fill", "#E8B5CE");
+    }
+    else if (clicked_id == "colorSoybean2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#D2C29D");
+        document.getElementById("_RightTop_").setAttribute("fill", "#D2C29D");
+    }
+    else if (clicked_id == "colorEclipse2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#343148");
+        document.getElementById("_RightTop_").setAttribute("fill", "#343148");
+    }
+    else if (clicked_id == "colorSweetCorn2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#F0EAD6");
+        document.getElementById("_RightTop_").setAttribute("fill", "#F0EAD6");
+    }
+    else if (clicked_id == "colorBrownGranite2") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#615550");
+        document.getElementById("_RightTop_").setAttribute("fill", "#615550");
+    }
+    else if (clicked_id == "resetColorButtonLeft") {
+        document.getElementById("_LeftSide_").setAttribute("fill", "#ffffff");
+        document.getElementById("_LeftTop_").setAttribute("fill", "#ffffff");
+    }
+    else if (clicked_id == "resetColorButtonRight") {
+        document.getElementById("_RightSide_").setAttribute("fill", "#ffffff");
+        document.getElementById("_RightTop_").setAttribute("fill", "#ffffff");
+    }
+}
+//Change wall color end
+
+//Button text bold on mouse enter, back to normal on mouse leave
+$(document).ready(function(){
+    $(".colorButtons").mouseenter(function(){
+        $(this).css("font-weight", "bold");
+    });
+    $(".colorButtons").mouseleave(function(){
+        $(this).css("font-weight", "normal");
+    });
+});
+
+//Shadow over color buttons on mouse enter, off on mouse leave
+$(document).ready(function(){
+    $(".colorButtons").mouseenter(function(){
+        $(this).css("box-shadow", "0px 0px 20px #FFFFFF");
+    });
+    $(".colorButtons").mouseleave(function(){
+        $(this).css("box-shadow", "none");
+    });
+});
+
+//Color info text (color name + HEX code) visible when clicked, left side
+$(document).ready(function(){
+    $(".colorButtonsLeft").click(function(){
+        var divText = $(this).text();
+        $(".leftWallInfo").text(divText);
+    });
+});
+
+//Color info text (color name + HEX code) visible when clicked, right side
+$(document).ready(function(){
+    $(".colorButtonsRight").click(function(){
+        var divText = $(this).text();
+        $(".rightWallInfo").text(divText);
+    });
+});
+
+//reset left wall color-info text box
+$(document).ready(function(){
+    $("#resetColorButtonLeft").click(function(){
+        $(".leftWallInfo").empty();
+    });
+});
+
+//reset right wall color-info text box
+$(document).ready(function(){
+    $("#resetColorButtonRight").click(function(){
+        $(".rightWallInfo").empty();
+    });
+});
+
+//Shadow on gallery thumbnails on mouse enter, off on mouse leave
+$(document).ready(function(){
+    $(".galleryImg").mouseenter(function(){
+        $(this).css("box-shadow", "0px 0px 20px #FFFFFF");
+    });
+    $(".galleryImg").mouseleave(function(){
+        $(this).css("box-shadow", "none");
+    });
+});
+
+//Alert when clickin form submit button
+function formSubmitAlert() {
+    alert("Viesti lähetetty!");
+}
